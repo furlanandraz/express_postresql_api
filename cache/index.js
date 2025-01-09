4f0c3e7d086de648aec0ca8997a3743190671adc
furlanandraz <andraz.furlan98@gmail.com>
2025-01-09 18:54:27 +0100

    cache listening service dir refactored to follow db schema structure

--- cache/index.js
+++ cache/index.js
@@ -1,30 +1,15 @@
-import createSubscriber from 'pg-listen';
-import { god } from '#clients';
+import navigationSubscriberInit from './navigation/index.js';
 
-const subscriber = createSubscriber({
-    pgClient: god
-});
-
-subscriber.notifications.on('menu_item_change', (payload) => {
-    console.log('Received menu item change notification:', payload);
-});
-
-subscriber.events.on('error', (error) => {
-    console.error('Fatal database connection error:', error);
-    process.exit(1);
-});
+(async () => {
+    try {
+        await navigationSubscriberInit();
+        console.log('Subscriber initialized.');
+    } catch (err) {
+        console.error('Error in root initialization:', err);
+    }
+})();
 
 process.on('exit', async () => {
     console.log('Closing subscriber...');
     await subscriber.close();
 });
-
-(async () => {
-    try {
-        await subscriber.connect();
-        console.log('Listening for notifications on channel: menu_item_change...');
-        await subscriber.listenTo('menu_item_change');
-    } catch (err) {
-        console.error('Error setting up subscriber:', err);
-    }
-})();
---
+++ cache/navigation/index.js
@@ -0,0 +1,27 @@
+import createSubscriber from 'pg-listen';
+import { god } from '#clients';
+
+const subscriber = createSubscriber({
+    pgClient: god
+});
+
+subscriber.notifications.on('menu_item_change', (payload) => {
+    console.log('Received menu item change notification:', payload);
+});
+
+subscriber.events.on('error', (error) => {
+    console.error('Fatal database connection error:', error);
+    process.exit(1);
+});
+
+async function subscriberInit () {
+    try {
+        await subscriber.connect();
+        console.log('Listening for notifications on channel: menu_item_change...');
+        await subscriber.listenTo('menu_item_change');
+    } catch (err) {
+        console.error('Error setting up subscriber:', err);
+    }
+}
+
+export default subscriberInit;
